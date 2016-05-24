d22f7410a3b243c90f8228825baa71d158a1b020
parthbathia <parthbathia@gmail.com>
2016-05-24 23:43:13 +0530

    Add files via upload

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,13 @@
+class SumOfTwoNumbers{
+
+	public static void main(String[] args){
+
+		int a = 10;
+		float b = 15.6f;
+	
+		int c = (int) (a + b);
+
+		System.out.println("The addition of numbers is : " + c);
+	}
+
+}
