cbed8b4d96e925d175c6e592d0fd6e766d8550cd
KRIDAY PATEL <66963931+DRACAA@users.noreply.github.com>
2020-10-08 16:50:16 +0530

    Update SumOfTwoNumbers.java

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
 class SumOfTwoNumbers{
 
 	public static void main(String[] args){
-
-		int a = 10;
-		float b = 15.6f;
-	
-		int c = (int) (a + b);
-
+		// Scanner is used for User-Input In Java
+              Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your first number")      
+		int a = sc.nextInt();
+		System.out.println("Enter your second number")
+                int b = sc.nextInt();
+		int c = a+b;
 		System.out.println("The addition of numbers is : " + c);
 	}
 
